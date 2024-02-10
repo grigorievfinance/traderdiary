cbf36695130da8940e194c59b0370bb50b69d829
Andrew <grigorievfinance@gmail.com>
2024-02-10 14:27:24 +0200

    Update TraderDiary

---
+++ src/main/java/com/grigorievfinance/traderdiary/Main.java
@@ -0,0 +1,11 @@
+package com.grigorievfinance.traderdiary;
+
+/**
+ * @see <a href="http://traderdiary.herokuapp.com">Demo application</a>
+ * @see <a href="https://github.com/girigorievfinance/traderdiary">Initial project</a>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Trader Diary Enterprise!");
+    }
+}
