a4a467a72952a25decb0236a1a4d4fc9946f41c7
Andrew <grigorievfinance@gmail.com>
2024-02-10 16:11:28 +0200

    simple logging

--- src/main/java/com/grigorievfinance/traderdiary/web/UserServlet.java
+++ src/main/java/com/grigorievfinance/traderdiary/web/UserServlet.java
@@ -1,14 +1,19 @@
 package com.grigorievfinance.traderdiary.web;
 
-import javax.servlet.ServletException;
+import org.slf4j.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        log.debug("redirect to users");
+        response.sendRedirect("users.jsp");
     }
 }
