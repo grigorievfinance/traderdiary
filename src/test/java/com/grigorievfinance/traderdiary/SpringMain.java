c13bbc43713a9247e8d100ad94e1979472649695
Andrew <grigorievfinance@gmail.com>
2024-02-24 16:20:11 +0200

    fix spring-app.xml

--- src/test/java/com/grigorievfinance/traderdiary/SpringMain.java
+++ src/test/java/com/grigorievfinance/traderdiary/SpringMain.java
@@ -10,9 +10,10 @@ import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("Bean definition names: " + Arrays.toString(applicationContext.getBeanDefinitionNames()));
-        AdminRestController adminRestController = applicationContext.getBean(AdminRestController.class);
-        adminRestController.create(new User(null, "username", "email@email.com", "password", Role.ADMIN));
+        try (ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-app.xml")){
+            System.out.println("Bean definition names: " + Arrays.toString(applicationContext.getBeanDefinitionNames()));
+            AdminRestController adminRestController = applicationContext.getBean(AdminRestController.class);
+            adminRestController.create(new User(null, "username", "email@email.com", "password", Role.ADMIN));
+        }
     }
 }
