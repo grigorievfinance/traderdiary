0e1770205a8d289e414b81142cbb00b38d740e81
Andrew <grigorievfinance@gmail.com>
2024-03-16 16:47:54 +0200

    DataJpaPositionRepository.java

--- src/main/java/com/grigorievfinance/traderdiary/repository/datajpa/CrudPositionRepository.java
+++ src/main/java/com/grigorievfinance/traderdiary/repository/datajpa/CrudPositionRepository.java
@@ -2,6 +2,24 @@ package com.grigorievfinance.traderdiary.repository.datajpa;
 
 import com.grigorievfinance.traderdiary.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CrudPositionRepository extends JpaRepository<Position, Integer>{
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Position p WHERE p.id=:id AND p.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Query("SELECT  p FROM Position p WHERE p.user.id=:userId ORDER BY p.dateTime DESC")
+    List<Position> getAll(@Param("userId") int userId);
+
+    @Query("SELECT p FROM Position p WHERE p.user.id=:userId AND p.dateTime>=:startDate AND p.dateTime<:endDate ORDER BY p.dateTime DESC")
+    List<Position> getBetweenHalfOpen(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
 }
--- src/main/java/com/grigorievfinance/traderdiary/repository/datajpa/DataJpaPositionRepository.java
+++ src/main/java/com/grigorievfinance/traderdiary/repository/datajpa/DataJpaPositionRepository.java
@@ -3,6 +3,7 @@ package com.grigorievfinance.traderdiary.repository.datajpa;
 import com.grigorievfinance.traderdiary.model.Position;
 import com.grigorievfinance.traderdiary.repository.PositionRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,33 +12,42 @@ import java.util.List;
 public class DataJpaPositionRepository implements PositionRepository {
 
     private final CrudPositionRepository crudPositionRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaPositionRepository(CrudPositionRepository crudPositionRepository) {
+    public DataJpaPositionRepository(CrudPositionRepository crudPositionRepository, CrudUserRepository crudUserRepository) {
         this.crudPositionRepository = crudPositionRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional
     public Position save(Position position, int userId) {
-        return null;
+        if (!position.isNew() && get(position.id(), userId) == null) {
+            return null;
+        }
+        position.setUser(crudUserRepository.getReferenceById(userId));
+        return crudPositionRepository.save(position);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudPositionRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Position get(int id, int userId) {
-        return null;
+        return crudPositionRepository.findById(id)
+                .filter(position -> position.getUser().getId() == userId)
+                .orElse(null);
     }
 
     @Override
     public List<Position> getAll(int userId) {
-        return null;
+        return crudPositionRepository.getAll(userId);
     }
 
     @Override
     public List<Position> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudPositionRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
