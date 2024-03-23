a30424659bc50d47efd79a26e14b5e2897fdc965
Andrew <grigorievfinance@gmail.com>
2024-03-23 15:55:22 +0200

    update hsqldb

--- src/main/java/com/grigorievfinance/traderdiary/repository/jdbc/JdbcPositionRepository.java
+++ src/main/java/com/grigorievfinance/traderdiary/repository/jdbc/JdbcPositionRepository.java
@@ -1,9 +1,7 @@
 package com.grigorievfinance.traderdiary.repository.jdbc;
 
-import com.grigorievfinance.traderdiary.Profiles;
 import com.grigorievfinance.traderdiary.model.Position;
 import com.grigorievfinance.traderdiary.repository.PositionRepository;
-import org.springframework.context.annotation.Profile;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,49 +11,21 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public abstract class JdbcPositionRepository<T> implements PositionRepository {
+@Repository
+public class JdbcPositionRepository implements PositionRepository {
 
     private static final RowMapper<Position> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Position.class);
     private final JdbcTemplate jdbcTemplate;
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final SimpleJdbcInsert insertPosition;
 
-    JdbcPositionRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    public JdbcPositionRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.insertPosition = new SimpleJdbcInsert(jdbcTemplate).withTableName("position").usingGeneratedKeyColumns("id");
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
-
-    protected abstract T toDbDateTime(LocalDateTime ldt);
-
-    @Repository
-    @Profile(Profiles.POSTGRES_DB)
-    public static class Java8JdbcPositionRepository extends JdbcPositionRepository<LocalDateTime> {
-        public Java8JdbcPositionRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-            super(jdbcTemplate, namedParameterJdbcTemplate);
-        }
-
-        @Override
-        protected LocalDateTime toDbDateTime(LocalDateTime ldt) {
-            return ldt;
-        }
-    }
-
-    @Repository
-    @Profile(Profiles.HSQL_DB)
-    public static class TimestampJdbcPositionRepository extends JdbcPositionRepository<Timestamp> {
-        public TimestampJdbcPositionRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-            super(jdbcTemplate, namedParameterJdbcTemplate);
-        }
-
-        @Override
-        protected Timestamp toDbDateTime(LocalDateTime ldt) {
-            return Timestamp.valueOf(ldt);
-        }
     }
 
     @Override
@@ -64,7 +34,7 @@ public abstract class JdbcPositionRepository<T> implements PositionRepository {
                 .addValue("id", position.getId())
                 .addValue("symbol", position.getSymbol())
                 .addValue("profit", position.getProfitLoss())
-                .addValue("date_time", toDbDateTime(position.getDateTime()))
+                .addValue("date_time", position.getDateTime())
                 .addValue("user_id", userId);
         if (position.isNew()) {
             Number newId = insertPosition.executeAndReturnKey(map);
@@ -97,6 +67,6 @@ public abstract class JdbcPositionRepository<T> implements PositionRepository {
     @Override
     public List<Position> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
         return jdbcTemplate.query("SELECT * FROM position WHERE user_id=? AND date_time >= ? AND date_time < ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, toDbDateTime(startDateTime), toDbDateTime(endDateTime));
+                ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
