238796a6415e46fbec42510e75119480f5bcf2c4
Andrew <grigorievfinance@gmail.com>
2024-04-20 16:34:34 +0300

    REST controllers

--- src/main/java/com/grigorievfinance/traderdiary/web/user/ProfileRestController.java
+++ src/main/java/com/grigorievfinance/traderdiary/web/user/ProfileRestController.java
@@ -1,18 +1,31 @@
 package com.grigorievfinance.traderdiary.web.user;
 
 import com.grigorievfinance.traderdiary.model.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import static com.grigorievfinance.traderdiary.web.SecurityUtil.authUserId;
 
-@Controller
+@RestController
+@RequestMapping(value = ProfileRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileRestController extends AbstractUserController {
+
+    static final String REST_URL = "/rest/profile";
+
+    @GetMapping
     public User get() {
         return super.get(authUserId());
     }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete() {
         super.delete(authUserId());
     }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(User user) {
         super.update(user, authUserId());
     }
