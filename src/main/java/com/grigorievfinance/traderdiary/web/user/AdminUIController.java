d0241a501debc4293696957df5ed6c34f5f4a0ee
Andrew <grigorievfinance@gmail.com>
2024-04-27 14:47:38 +0300

    AdminUIController.java

---
+++ src/main/java/com/grigorievfinance/traderdiary/web/user/AdminUIController.java
@@ -0,0 +1,34 @@
+package com.grigorievfinance.traderdiary.web.user;
+
+import com.grigorievfinance.traderdiary.model.Role;
+import com.grigorievfinance.traderdiary.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/admin/users", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminUIController extends AbstractUserController {
+    @Override
+    @GetMapping
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestParam String name,
+                       @RequestParam String email,
+                       @RequestParam String password) {
+        super.create(new User(null, name, email, password, Role.USER));
+    }
+}
